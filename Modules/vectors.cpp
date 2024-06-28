38a0f34e5f2196a1fd6bf699f710f20244e4a6a3
Mattia04 <mattia.ballico@gmail.com>
2024-06-28 12:20:48 +0200

    "moved files"

--- Modules/vectors.cpp
+++
@@ -1,6 +0,0 @@
-#include "vectors.h"
-
-int test()
-{
-    return 3;
-}
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Modules/vectors.h"
-
-using namespace std;
-
-int main()
-{
-    cout << test() << endl;
-}
